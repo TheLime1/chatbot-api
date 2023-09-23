5ae14981e0739da167d2b5647223f94985810a18
TheLime03 <everpadd4@gmail.com>
2023-09-23 18:12:09 +0100

    working base command

--- chatgpt.py
+++ chatgpt.py
@@ -9,15 +9,20 @@ from fastapi_poe.types import (
     SettingsResponse,
 )
 
-
 class ChatGPTBot(PoeBot):
+    base_prompt = "only speak arabic"  # Add your base prompt here
+
     async def get_response(self, query: QueryRequest) -> AsyncIterable[PartialResponse]:
+        # Prepend the base prompt to the user's query
+        query.query[0].content = self.base_prompt + query.query[0].content
         async for msg in stream_request(query, "ChatGPT", query.access_key):
             yield msg
-
 
     async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
         return SettingsResponse(
             allow_attachments=True,
             server_bot_dependencies={"ChatGPT": 1}
-    )
+        )
+
+if __name__ == "__main__":
+    run(ChatGPTBot)
